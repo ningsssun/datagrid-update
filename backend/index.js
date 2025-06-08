351d2fec601ccd1d4bdaf5c727d993a789907d15
sunning <ning.sssun@gmail.com>
2025-06-08 18:04:28 +0200

    add back end url

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,6 @@ app.use('/api/cars', carRoutes);
 // Serve the React static files
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-// Catch-all route for React
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
